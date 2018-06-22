a868250a314ce4f3c674386c39946139f998f1fa
Tarun Talreja <tarun.talreja@toppr.com>
2018-06-22 16:23:57 +0530

    Dict and set files added

---
+++ Python Basics/Data Structures/set.py
@@ -0,0 +1,21 @@
+a = set([1, 2, 3])
+b = set([2, 3, 4, 5])
+
+#different methods on set
+
+c = a.intersection(b)
+print c
+
+print c.issubset(a)
+print c.issuperset(a)
+print c >= a
+
+print a.difference(b)
+print a - b
+
+print a.symmetric_difference(b)
+print a^b
+
+#copy a set
+x = a.copy()
+print x
